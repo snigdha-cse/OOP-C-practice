56c2321be8b5cb5991f62c82a421dd672c75400e
snigdha-cse <snigdhamridha31@gmail.com>
2026-09-09 01:08:24 +0900

    add practice2 code

---
+++ practice.cpp/practice2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"x:";
+    cin>>x;
+    cout<<"y:";
+    cin>>y;
+    if(x>y)
+        cout<<"x is greater than y"<<endl;
+    else if(x<y)
+        cout<<"x is less than y"<<endl;
+  return 0;
+}
