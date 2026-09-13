a2352be986b7453b5b6e312661524b07e316e0a9
snigdha-cse <snigdhamridha31@gmail.com>
2026-09-13 13:03:46 +0900

    add practice6 code

---
+++ practice.cpp/practice6.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Teacher{
+private:
+    double salary;
+public:
+    //properties
+    string name;
+    string dept;
+    string subject;
+   
+
+    //method/member functions
+    void changeDept(string newDept){
+        dept=newDept;
+    }
+    void setSalary(double s){
+        salary=s;
+    }
+    double getSalary(){
+        return salary;
+    }
+};
+int main(){
+    Teacher t1;
+    t1.name="snigdha";
+    t1.subject="c++";
+    t1.dept="cse";
+    t1.setSalary(50000);
+   
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+    return 0;
+}
