7de62d0624203d96ab3a30bdbacd258a8134ebfe
snigdha-cse <snigdhamridha31@gmail.com>
2026-09-16 01:27:17 +0900

    add practice20 code

---
+++ practice.cpp/practice20.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s1="Slow", s2="Steady";
+    string s3="wins the race";
+    string s4;
+    s4=s1+s2+s3;
+    cout<<s4<<endl;
+    return 0;
+}
