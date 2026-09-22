d24ccfb4c5211d94a5c4d80c8dc435d150111e0b
snigdha-cse <snigdhamridha31@gmail.com>
2026-09-22 22:39:54 +0900

    add practice30 code

---
+++ practice.cpp/practice30.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Pizza{
+public:
+      Pizza(int s): size(s){}
+      int size;
+};
+void makeDouble(Pizza p){
+    p.size *=2;
+
+}
+int main(){
+    Pizza pizza(10);
+    makeDouble(pizza);
+    cout<<pizza.size<<"inch pizza"<<endl;
+    return 0;
+}
